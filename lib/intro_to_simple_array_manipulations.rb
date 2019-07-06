d272773e63986a3ad10e2c68e4df2c16499b4047
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-06 19:04:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -50,12 +50,12 @@ def using_flatten(instruments)
   p flat_array
 end
 
-def using_delete(instructors)
+def using_delete(instructors, "Steven")
   no_offense_steven = instructors.delete("Steven")
   p no_offense_steven
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, 2)
   deleted_robot = famous_robots.delete_at(2)
   p deleted_robot
 end
